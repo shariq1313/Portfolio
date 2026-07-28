1759ddf0d27b5621ce6e4b05c341bd779318a12b
shariq1313 <abdulshariq345@gmail.com>
2026-07-28 22:26:03 +0500

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,15 @@
+/*====================================
+        LOADER
+====================================*/
+
+window.addEventListener("load",()=>{
+
+    const loader=document.querySelector(".loader");
+
+    setTimeout(()=>{
+
+        loader.classList.add("hide");
+
+    },2500);
+
+});
